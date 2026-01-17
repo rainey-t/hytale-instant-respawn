117f907189dbda3362c3300ab30646411f85b80c
Julian Vagg <julianvagg4@gail.com>
2026-01-17 03:32:54 -0600

    Instant Respawn System

--- src/main/java/dev/hytalemodding/systems/DeathSystem.java
+++ src/main/java/dev/hytalemodding/systems/DeathSystem.java
@@ -3,19 +3,26 @@ package dev.hytalemodding.systems;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.dependency.Dependency;
+import com.hypixel.hytale.component.dependency.Order;
+import com.hypixel.hytale.component.dependency.SystemDependency;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.protocol.packets.interface_.Page;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.entity.entities.player.pages.PageManager;
+import com.hypixel.hytale.server.core.entity.entities.player.pages.RespawnPage;
 import com.hypixel.hytale.server.core.modules.entity.damage.Damage;
 import com.hypixel.hytale.server.core.modules.entity.damage.DeathComponent;
 import com.hypixel.hytale.server.core.modules.entity.damage.DeathSystems;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.Universe;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Set;
 
 public class DeathSystem extends DeathSystems.OnDeathSystem {
 
@@ -24,19 +31,26 @@ public class DeathSystem extends DeathSystems.OnDeathSystem {
         return Player.getComponentType();
     }
 
+    @Nonnull
+    @Override
+    public Set<Dependency<EntityStore>> getDependencies() {
+        return Set.of(new SystemDependency<>(Order.BEFORE, DeathSystems.PlayerDeathScreen.class));
+    }
+
     @Override
     public void onComponentAdded(@Nonnull Ref ref, @Nonnull DeathComponent component, @Nonnull Store store, @Nonnull CommandBuffer commandBuffer) {
 
         Damage deathInfo = component.getDeathInfo();
         Player playerComponent = (Player) store.getComponent(ref, Player.getComponentType());
+
         if (playerComponent == null || deathInfo == null){
             return;
         }
-        World world = playerComponent.getWorld();
-        if (world != null) {
-            playerComponent.getWorld().execute(() -> {
-                playerComponent.getPageManager().setPage(ref, store, Page.None);
-            });
-        }
+
+        component.setDeathMessage(Message.raw("Subaru"));
+
+        assert playerComponent != null;
+
+        store.tryRemoveComponent(ref, DeathComponent.getComponentType());
     }
 }
