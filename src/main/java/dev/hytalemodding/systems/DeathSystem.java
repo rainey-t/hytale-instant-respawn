3dd2d4ddeb99b30f62e9b21cfca8ea031169005a
Julian Vagg <julianvagg4@gail.com>
2026-01-16 19:24:55 -0600

    Create death event system

---
+++ src/main/java/dev/hytalemodding/systems/DeathSystem.java
@@ -0,0 +1,23 @@
+package dev.hytalemodding.systems;
+
+import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.query.Query;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.modules.entity.damage.DeathComponent;
+import com.hypixel.hytale.server.core.modules.entity.damage.DeathSystems;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class DeathSystem extends DeathSystems.OnDeathSystem {
+    @Nullable
+    public Query getQuery() {
+        return Player.getComponentType();
+    }
+
+    public void onComponentAdded(@Nonnull Ref ref, @Nonnull DeathComponent component, @Nonnull Store store, @Nonnull CommandBuffer commandBuffer) {
+        Player player = (Player) store.getComponent(ref, Player.getComponentType());
+    }
+}
