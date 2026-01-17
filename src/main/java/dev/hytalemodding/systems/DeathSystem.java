aa2902138a9b37a001b6f32b2d293c7f9f65b05d
Julian Vagg <julianvagg4@gail.com>
2026-01-16 19:46:09 -0600

    Finish death system event

--- src/main/java/dev/hytalemodding/systems/DeathSystem.java
+++ src/main/java/dev/hytalemodding/systems/DeathSystem.java
@@ -4,9 +4,11 @@ import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.component.query.Query;
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.modules.entity.damage.DeathComponent;
 import com.hypixel.hytale.server.core.modules.entity.damage.DeathSystems;
+import com.hypixel.hytale.server.core.universe.Universe;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -19,5 +21,7 @@ public class DeathSystem extends DeathSystems.OnDeathSystem {
 
     public void onComponentAdded(@Nonnull Ref ref, @Nonnull DeathComponent component, @Nonnull Store store, @Nonnull CommandBuffer commandBuffer) {
         Player player = (Player) store.getComponent(ref, Player.getComponentType());
+        assert player != null;
+        player.sendMessage(Message.raw("Test"));
     }
 }
